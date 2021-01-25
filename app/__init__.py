e83db82100d79b2239f5022b9624d844dcba5d77
Jerzy <jerzyhorst@gmail.com>
2021-01-25 12:28:39 -0800

    models in progress

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
 
-from .models import db, User
+from .models import db, User, Location, Booking, Review, Photo
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 
@@ -14,6 +14,7 @@ from .seeds import seed_commands
 from .config import Config
 
 app = Flask(__name__)
+
 
 # Setup login manager
 login = LoginManager(app)
@@ -32,7 +33,7 @@ app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 db.init_app(app)
-Migrate(app, db)
+migrate = Migrate(app, db)
 
 # Application Security
 CORS(app)
