8254c435c52f2e31db3a911e5563b729c6c19b3b
Arianna Johnson <ariannajohnson588@gmail.com>
2021-01-26 11:57:46 -0500

    finished route

--- app/api/location_routes.py
+++ app/api/location_routes.py
@@ -8,6 +8,5 @@ location_routes = Blueprint('location', __name__)
 @location_routes.route('/')
 # @login_required
 def locations():
-    return "locations"
-    # locations = Location.query.all()
-    # return {"locations": [location.to_dict() for location in locations]}
+    locations = Location.query.all()
+    return {"locations": [location.to_dict() for location in locations]}
