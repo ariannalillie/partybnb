55e3afa93b016742b21da6e08212f1ee1288e699
Rihana Chang <martinson.r@gmail.com>
2021-01-27 11:02:32 -0800

    Add functionality to link current user with created listing

--- app/api/location_routes.py
+++ app/api/location_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request, redirect
 from flask_login import login_required
-from app.models import db, Location
+from app.models import db, Location, User
+from flask_login import current_user
 
 location_routes = Blueprint('location', __name__)
 
@@ -20,7 +21,8 @@ def individualListing(id):
 def addListing():
     form_data = request.get_json(force=True)
     print('FORM DATA', form_data)
-    new_listing = Location(users_OwnerId=1,title=form_data["title"],venueType=form_data["venueType"], description=form_data["description"], amenities=form_data["amenities"], maxGuests=form_data["maxGuests"], bookingPrice=form_data["bookingPrice"], address=form_data["address"], city=form_data["city"], state=form_data["state"], zipcode=form_data["zipcode"])
+    print('USER ID', current_user.id)
+    new_listing = Location(users_OwnerId=current_user.id,title=form_data["title"],venueType=form_data["venueType"], description=form_data["description"], amenities=form_data["amenities"], maxGuests=form_data["maxGuests"], bookingPrice=form_data["bookingPrice"], address=form_data["address"], city=form_data["city"], state=form_data["state"], zipcode=form_data["zipcode"])
     db.session.add(new_listing)
     db.session.commit()
     return redirect('/')
