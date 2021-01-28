c7dc37a1a3eafa40a625009f10fdd42138bf4b65
Rihana Chang <martinson.r@gmail.com>
2021-01-28 13:08:41 -0800

    Set up search from home page to display listings on locations page

--- react-app/src/components/Search/index.js
+++ react-app/src/components/Search/index.js
@@ -2,11 +2,10 @@ import React, { useState, useEffect } from "react";
 import './Search.css';
 import {searchLocations} from "../../store/locations";
 import { useSelector, useDispatch } from "react-redux";
-import search from "../../media/search.svg"
+import {  useHistory } from "react-router-dom";
+import search from "../../media/search.svg";
 
 // import Location from "../../../../app/models/";
-
-
 
 const fakeData = {
   address: "52 Prentiss St, Watertown, MA 02472",
@@ -16,7 +15,8 @@ const fakeData = {
   bookingPrice: "500",
 };
 const Search = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const history = useHistory();
   const [locations, setLocations] = useState("")
   const [queryLocation, setQueryLocation] = useState("")
 
@@ -26,36 +26,15 @@ const Search = () => {
 
   const submitForm = async (e) => {
     e.preventDefault();
+
     let spaceRemover = queryLocation.split(" ").join("%20")
     if(numGuests == null || numGuests <1){
       numGuests = 1
     }
     const payload = {spaceRemover, checkInDate, checkOutDate, numGuests}
-    dispatch(searchLocations(payload))
-
-
-
-    // const res = await fetch(
-    //   `https://google-maps-geocoding.p.rapidapi.com/geocode/json?address=${spaceRemover}`,
-    //   {
-    //     method: "GET",
-    //     headers: {
-    //       "x-rapidapi-key":
-    //         "20dde32ademsha97b6dc9dd8189bp1973e4jsnbc1294ead8c1",
-    //       "x-rapidapi-host": "google-maps-geocoding.p.rapidapi.com",
-    //     },
-    //   }
-    // );
-    // const googleResponse = await res.json()
-
-    // let searchLat = await googleResponse.results[0].geometry.location.lat
-    // let searchLng = await googleResponse.results[0].geometry.location.lng
-    // // // let searchbuffer = 0.08 // 69mi/1deg
-    // const locationsNearSearchArea = await fetch(`http://localhost:5000/api/location/proximity/${searchLat}/${searchLng}`)
-
-    // console.log("just work...     ", searchLat, searchLng);
+    dispatch(searchLocations(payload));
+    history.push('/locations')
   };
-
 
   useEffect(() => {
     const fetchLocations = async () =>{
@@ -65,14 +44,6 @@ const Search = () => {
     }
     fetchLocations();
   }, []);
-
-
-  // useEffect(async () => {
-  //   const res = await fetch(`http://localhost:5000/api/location`)
-  //   const data = await res.json();
-  //   setLocations(data)
-  // }, []);
-
 
   return (
     <div className="location">
