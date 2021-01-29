116491e956fc40d914339c012333b2ac4466aba3
Tanner Schmutte <tannerschmutte@gmail.com>
2021-01-28 14:42:26 -1000

    navbar conditioned on isAuthenticated

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,36 +2,52 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 
-const NavBar = ({ setAuthenticated }) => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/" exact={true} activeClassName="active">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" exact={true} activeClassName="active">
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/users" exact={true} activeClassName="active">
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton setAuthenticated={setAuthenticated} />
-        </li>
-      </ul>
-    </nav>
-  );
-}
+const NavBar = ({ authenticated, setAuthenticated }) => {
+    console.log('is authenticated:       ', authenticated);
+
+    console.log('set authenticated:       ', setAuthenticated);
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="/" exact={true} activeClassName="active">
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    {!authenticated && 
+                        <NavLink
+                            to="/login"
+                            exact={true}
+                            activeClassName="active"
+                        >
+                            Login
+                        </NavLink>
+                    }
+                </li>
+                <li>
+                    {!authenticated && 
+                        <NavLink
+                            to="/sign-up"
+                            exact={true}
+                            activeClassName="active"
+                        >
+                            Sign Up
+                        </NavLink>
+                    }
+                </li>
+                <li>
+                    <NavLink to="/users" exact={true} activeClassName="active">
+                        Users
+                    </NavLink>
+                </li>
+                <li>
+                    <LogoutButton setAuthenticated={setAuthenticated} />
+                </li>
+            </ul>
+        </nav>
+    );
+};
 
 export default NavBar;
